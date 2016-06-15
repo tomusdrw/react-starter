043a9884b8ce5d43e5d244c2eba45a182e24cd00
Tomusdrw <tomusdrw@gmail.com>
2016-06-15 10:29:56 +0200

    CSS Modules

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,7 @@ module.exports = {
       }
     }, {
       test: /\.css$/,
+      // loaders: ['style-loader', 'css-loader?modules']
       loaders: ['style-loader', 'css-loader']
     }, {
       test: /\.html$/,
